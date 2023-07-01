f079502b39b94be95f95cad1b887f680e60b8493
Guilherme Braga <braguil@gmail.com>
2023-07-01 15:37:51 -0300

    Configurado todos os links

---
+++ assets/javascript/experiencia-button.js
@@ -0,0 +1,7 @@
+function redirecionarRepositorio() {
+    window.location.href = 'https://github.com/braguilweb/-Desafio-Challenge-ONE-Alura-Decodificador-de-textos'
+}
+
+function redirecionarDemo() {
+    window.location.href = 'https://desafio-challenge-one-alura-decodificador-de-textos.vercel.app/'
+}
